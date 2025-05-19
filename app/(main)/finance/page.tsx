36e79f58b70da9dc82358d947129a5e0410c3607
sankalpaacharya <sankalpaacharya01@gmail.com>
2025-05-20 00:14:52 +0530

    feat(ui): add accordion

--- app/(main)/finance/page.tsx
+++ app/(main)/finance/page.tsx
@@ -7,6 +7,12 @@ import {
 } from "@/components/ui/popover";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 type Props = {};
 
@@ -14,7 +20,7 @@ export default function Page({}: Props) {
   return (
     <div>
       <TotalAmountStatus amount={2889} />
-      <div className="flex justify-between">
+      <div className="flex justify-between mb-6">
         <Popover>
           <PopoverTrigger>
             <div className="flex gap-2 items-center text-blue-300 p-2 px-3 hover:bg-blue-500/10 rounded-md cursor-pointer">
@@ -40,6 +46,7 @@ export default function Page({}: Props) {
           </PopoverContent>
         </Popover>
       </div>
+      <BudgetTable />
     </div>
   );
 }
@@ -55,5 +62,41 @@ function TotalAmountStatus({ amount }: { amount: number }) {
         <Button className="shadow-xl cursor-pointer">Assign</Button>
       </div>
     </div>
+  );
+}
+
+function BudgetTable() {
+  return (
+    <div className="space-y-3">
+      <div className="flex justify-between bg-white/10 p-3 pr-5 rounded-md">
+        <span>Category</span>
+        <div className="flex">
+          <span className="w-26">Assigned</span>
+          <span className="w-26">Activity</span>
+          <span className="w-26">Available</span>
+        </div>
+      </div>
+      <CategoryGroup />
+    </div>
+  );
+}
+
+function CategoryGroup() {
+  return (
+    <Accordion type="single" collapsible className="w-full">
+      <AccordionItem value="item-1" className="border-none">
+        <AccordionTrigger className="hover:no-underline bg-white/10 p-3 rounded-t-md rounded-b-none border-b-1 font-semibold text-md">
+          Savings
+        </AccordionTrigger>
+        <AccordionContent className="bg-white/10 p-3 rounded-b-md rounded-t-none px-4 text-lg flex justify-between">
+          <span>💻 TV, Phone, Internet</span>
+          <div className="flex">
+            <span className="w-26">100</span>
+            <span className="w-26">100</span>
+            <span className="w-26">100</span>
+          </div>
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
   );
 }
