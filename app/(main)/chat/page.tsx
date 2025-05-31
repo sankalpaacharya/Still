8cdc72f56dbd0ff38c8cc78b80ba5606d9e542ab
sankalpaacharya <sankalpaacharya01@gmail.com>
2025-05-31 22:18:02 +0530

    feat: add markdown render, sse

--- app/(main)/chat/page.tsx
+++ app/(main)/chat/page.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import ChatInput from "@/features/chat/components/chat-input";
+import Conversation from "@/features/chat/components/coversation";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 type Props = {};
 
-export default function Page({}: Props) {
+export default function Page() {
   return (
     <div className="flex flex-col h-full">
-      <div className="flex-1"></div>
+      <ScrollArea className="flex-1  h-[800px] p-10">
+        <Conversation isUser={false} />
+      </ScrollArea>
       <ChatInput />
     </div>
   );
